bdb1709b20c0356644ce2de79e88bdd633ce63c5
ThatMajesticGuy <31870093+ThatMajesticGuy@users.noreply.github.com>
2018-06-30 12:26:06 -0500

    Update qna.js

--- commands/qna.js
+++ commands/qna.js
@@ -11,7 +11,7 @@ exports.run = (bot, message, args) => {
   .setDescription("This is a list of questions and answers that users have asked or may ask, if your question is not here, ask a moderator for an answer.")
   .addField("How do you install CTGP?", "You can install it from [Here](http://www.chadsoft.co.uk/), and you can follow the install guide from there")
   .addField("How do I get to the leaderboards?", "You have to beat the records of everyone on the specified track you want to set a record in. This will get you on [this leaderboard](http://www.chadsoft.co.uk/time-trials/)")
-  msg.channel.send({embed: embed})
+  message.channel.send({embed: embed})
 }
 
 exports.help = {
